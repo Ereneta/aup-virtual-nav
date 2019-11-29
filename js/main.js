6e00afd6897b57c65708f7b016a213a00c5cf79d
FrannieJames <ballenasfra.ne08@gmail.com>
2019-11-30 02:30:05 +0800

    Adding function for Options navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
+function closeNav() {
+    document.getElementById("nav-a").style.width = "0";
+}
+
+function openNav() {
+    document.getElementById("nav-a").style.width = "250px";
+}
+
+
+
 let date = new Date();
 let yearPos = document.getElementById("getYear");
 let currYear = date.getFullYear();
