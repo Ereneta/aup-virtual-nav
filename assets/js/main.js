5082a04f144d9a6acfde79db37d8f0fec9fc2cd7
FrannieJames <ballenasfra.ne08@gmail.com>
2019-12-02 21:01:52 +0800

    remove redundant codes

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,4 @@ const MDCTextField = mdc.textField.MDCTextField;
 const buttonRipple = mdc.ripple.MDCRipple;
 const button = new buttonRipple(document.querySelector('.mdc-button--raised'));
 const mdctext = new MDCTextField(document.querySelector('.mdc-text-field'));
-const mdctextarea = new MDCTextField(document.querySelector('.mdc-text-field--textarea')); 
-
-let date = new Date();
-let yearPos = document.getElementById("getYear");
-let currYear = date.getFullYear();
-yearPos.innerHTML = currYear;
+const mdctextarea = new MDCTextField(document.querySelector('.mdc-text-field--textarea'));
