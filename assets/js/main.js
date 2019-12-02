cf4468104d17a80265603c643803212b54d037d5
Fritz Ereneta <fereneta885@gmail.com>
2019-12-02 20:48:37 +0800

    Reintroduce JS code for forms

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,10 @@ let date = new Date();
 let yearPos = document.getElementById("getYear");
 let currYear = date.getFullYear();
 yearPos.innerHTML = currYear;
+
+// Form JS
+const MDCTextField = mdc.textField.MDCTextField;
+const buttonRipple = mdc.ripple.MDCRipple;
+const button = new buttonRipple(document.querySelector('.mdc-button--raised'));
+const mdctext = new MDCTextField(document.querySelector('.mdc-text-field'));
+const mdctextarea = new MDCTextField(document.querySelector('.mdc-text-field--textarea')); 
